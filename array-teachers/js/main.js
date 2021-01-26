6b33b7218a4ad1f683eb40e78e5fb81274787651
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-01-26 10:01:18 +0100

    array-teachers added

---
+++ array-teachers/js/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+let teachers = [{
+  name: "Birgitte Kirk Iversen",
+  initials: "bki",
+  mail: "bki@baaa.dk",
+  phone: "72286316",
+  address: "Sønderhøj 30, 8260 Viby J",
+  position: "Senior Lecturer",
+  department: "Multimedia Design",
+  img: "https://www.baaa.dk/media/u4gorzsd/birgitte-kirk-iversen2.jpg"
+},
+{
+  name: "Rasmus Cederdorff",
+  initials: "race",
+  mail: "race@baaa.dk",
+  phone: "72286318",
+  address: "Sønderhøj 30, 8260 Viby J",
+  position: "Lecturer",
+  department: "Multimediedesigner & Professionsbachelor i digital konceptudvikling",
+  img: "https://www.baaa.dk/media/devlvvgj/rasmus-cederdorff.jpg"
+}
+];
+
+console.log(teachers);
+appendTeachers(teachers);
+
+// Appending objects to the DOM
+function appendTeachers(teachers) {
+  for (let teacher of teachers) {
+    console.log(teacher);
+    document.querySelector("#grid-teachers").innerHTML +=/*html*/ `
+    <article>
+      <img src='${teacher.img}'>
+      <h3>${teacher.name}</h3>
+      ${teacher.position}<br>
+      <a href='mailto:${teacher.mail}'>${teacher.mail}</a>
+    </article>`;
+  }
+}
