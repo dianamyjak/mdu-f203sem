de340e63bb7a31961c99c6aecdfb023bff9157cb
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-02-04 13:32:39 +0100

    Update functions.php

--- wp-astra-child/functions.php
+++ wp-astra-child/functions.php
@@ -18,7 +18,7 @@ define( 'CHILD_THEME_ASTRA_CHILD_VERSION', time() );
  */
 function child_enqueue_styles() {
 	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION);
-    wp_enqueue_script('astra-child-theme-js', get_stylesheet_directory_uri() . '/script.js', array('typed-js'), CHILD_THEME_ASTRA_CHILD_VERSION);
+    wp_enqueue_script('astra-child-theme-js', get_stylesheet_directory_uri() . '/script.js', array(), CHILD_THEME_ASTRA_CHILD_VERSION);
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles');
